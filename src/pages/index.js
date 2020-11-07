64ae025ea9648fa39efa5197805d75d66d75f8d3
Lucas Caton <lucascaton@gmail.com>
2020-11-07 12:13:38 +1000

    Create Next.js project

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Contrata-se devs</title>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </Head>
+
+      <main>
+        <h1>Contrata-se devs</h1>
+      </main>
+
+      <footer>https://github.com/lucascaton/contrata-se-devs</footer>
+    </>
+  );
+}
