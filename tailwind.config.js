e29d808deb452a4c150e3bc83a03a67526cc28df
Lucas Caton <lucascaton@gmail.com>
2021-02-10 15:41:21 +1000

    Upgrade Tailwind CSS to version 2.x
    https://tailwindcss.com/docs/upgrading-to-v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  plugins: [require("@tailwindcss/ui"), require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography")],
   purge: ["./src/components/**/*.{js,ts,jsx,tsx}", "./src/pages/**/*.{js,ts,jsx,tsx}"],
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-    defaultLineHeights: true,
-    standardFontWeights: true,
-  },
 };
